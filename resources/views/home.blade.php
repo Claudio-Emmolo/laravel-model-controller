2c934d3a84bd6beffdcb6ac59fa7e5711dd97b0d
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-02-09 15:44:51 +0100

    Change "foreach" to "forelse"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,22 +6,24 @@
 <main>
     <div class="container">
         <div class="row align-items-stretch row-cols-1 row-cols-lg-3">
-            @foreach ($movies as $movie)
-                <div class="col g-4">
-                    <div class="movie-card card d-flex justify-content-center text-center">
-                        <h2>Titolo: 
-                            <span class="fw-bold fs-3">
-                                {{$movie->title}} </h2>
-                            </span>
-                        <h4>Titolo Originale: {{$movie->original_title}}</h4>
-                        <p>
-                            Nazionalità: {{$movie->nationality}} <br>
-                            Data di uscita: {{$movie->date}} <br>
-                            Voto: {{$movie->vote}}
-                        </p>
-                    </div>
+            @forelse ($movies as $movie)
+            <div class="col g-4">
+                <div class="movie-card card d-flex justify-content-center text-center">
+                    <h2>Titolo: 
+                        <span class="fw-bold fs-3">
+                            {{$movie->title}} </h2>
+                        </span>
+                    <h4>Titolo Originale: {{$movie->original_title}}</h4>
+                    <p>
+                        Nazionalità: {{$movie->nationality}} <br>
+                        Data di uscita: {{$movie->date}} <br>
+                        Voto: {{$movie->vote}}
+                    </p>
                 </div>
-            @endforeach
+            </div>
+            @empty
+                <p class="text-light">Nessun Film Disponibile</p>
+            @endforelse
         </div>
     </div>
 </main>

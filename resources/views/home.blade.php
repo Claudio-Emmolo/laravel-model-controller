3e715a724b53f92d2027ade2464b059aa96888b8
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-02-09 15:02:20 +0100

    Import DB on my home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,13 @@
 @section('title', 'Home')
 
 @section('main-app')
+@foreach ($movies as $movie)
+    @dump($movie->title)
+    @dump($movie->original_title)
+    @dump($movie->nationality)
+    @dump($movie->date)
+    @dump($movie->vote)
 
+    <hr>
+@endforeach
 @endsection
